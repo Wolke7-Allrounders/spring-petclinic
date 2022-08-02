87133dac695df7bb59ee46d59ed10ac104a5831e
JKI <JKI@opitz-consutlting.com>
2022-08-02 09:31:04 +0000

    add terraform scripts 4

---
+++ terraform/auto_scaling.tf
@@ -0,0 +1,51 @@
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.test-cluster.name}/${aws_ecs_service.test-service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+
+resource "aws_launch_configuration" "ecs_launch_config" {
+    image_id             = "ami-094d4d00fd7462815"
+    iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
+    security_groups      = [aws_security_group.ecs_sg.id]
+    user_data            = "#!/bin/bash\necho ECS_CLUSTER=myapp-cluster>> /etc/ecs/ecs.config"
+    instance_type        = "t2.medium"
+}
+
+
+
+resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
+    name                      = "asg"
+    vpc_zone_identifier       = [aws_subnet.public[0].id]
+    launch_configuration      = aws_launch_configuration.ecs_launch_config.name
+
+    desired_capacity          = 2
+    min_size                  = 1
+    max_size                  = 10
+    health_check_grace_period = 300
+    health_check_type         = "EC2"
+}
+
+
+#Automatically scale capacity up by one
+resource "aws_appautoscaling_policy" "ecs_policy_up" {
+  name               = "scale-down"
+  policy_type        = "StepScaling"
+  resource_id        = "service/${aws_ecs_cluster.test-cluster.name}/${aws_ecs_service.test-service.name}"
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
---
+++ terraform/security-groups.tf
@@ -0,0 +1,63 @@
+# security group creation and attcahing in ecs, alb etc
+
+# ALB Security Group: Edit to restrict access to the application
+resource "aws_security_group" "alb-sg" {
+  name        = "testapp-load-balancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = aws_vpc.test-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# this security group for ecs - Traffic to the ECS cluster should only come from the ALB
+resource "aws_security_group" "ecs_sg" {
+  name        = "testapp-ecs-tasks-security-group"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.test-vpc.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port       = 443
+        to_port         = 443
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+ # egress {
+ #   protocol    = "-1"
+  #  from_port   = 0
+ #   to_port     = 0
+ #   cidr_blocks = ["0.0.0.0/0"]
+ # }
+}
